19b5ff604f05735681cb6b5f02691c56cc22b328
Aayushi Varma <aayuv17@gmail.com>
2025-04-15 17:31:26 +0530

    boilerplate

--- App.tsx
+++ App.tsx
@@ -5,6 +5,10 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>So an update was made!</Text>
+      <Text>Wow I am so excited!!</Text>
+      <Text>Hey! I think I have this now!! Yayyy</Text>
+      <Text>Omg it worked just like that! How crazy is that?</Text>
       <StatusBar style="auto" />
     </View>
   );
